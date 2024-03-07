85ba51d3ae7d22da17c327e8edf300cb2a31db39
Nigel Lai <nigel.lai132@gmail.com>
2024-03-08 12:53:17 +1300

    basic call

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -14,20 +14,10 @@ export const handler = async () => {
     const animeDetailsResponse = await fetch(
         `${API_URL}anime/52991?fields=id,title,main_picture,alternative_titles,start_date,synopsis,genres,start_season,source,rating,pictures`,
         options
-    );
+    ).then((res) => res.json());
 
-    const responseData = await animeDetailsResponse.json();
-
-    const headers = {
-        ...animeDetailsResponse.headers,
-        "Access-Control-Allow-Origin": "*", // Allow requests from any origin, replace * with specific origins if needed
-        "Access-Control-Allow-Methods": "GET,OPTIONS", // Allow GET and OPTIONS methods
-        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow headers Content-Type and Authorization
-    };
-
-    return {
-        statusCode: animeDetailsResponse.status,
-        headers: headers,
-        body: JSON.stringify(responseData),
-    };
+	return {
+		statusCode: 200,
+		body: JSON.stringify(animeDetailsResponse),
+	};
 };
