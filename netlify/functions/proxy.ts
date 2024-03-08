bf85ff7eae4b861039b69fac590f41aa61875f62
Nigel Lai <79781119+nlai659@users.noreply.github.com>
2024-03-08 15:58:00 +1300

    Deployed on Netlify - proxy serverless (#1)

---
+++ netlify/functions/proxy.ts
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+const API_CLIENT_ID = process.env.VITE_MAL_CLIENT_ID;
+
+const options: any = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+    "X-MAL-CLIENT-ID": API_CLIENT_ID,
+  },
+};
+
+export const handler = async (event) => {
+  const url = event.queryStringParameters.url;
+
+  try {
+    const response = await fetch(
+      url,
+      options
+    );
+    
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify(await response.json()),
+    };
+  } catch (error) {
+    return {
+	  headers: {
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Origin": "*",
+	  },
+	  statusCode: 500,
+	  body: JSON.stringify({ message: error.message }),
+	};
+  }
+};
+
+module.exports = { handler };
