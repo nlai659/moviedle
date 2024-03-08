d7c4d37b274e835e031637a601e55c689c278f17
Nigel Lai <nigel.lai132@gmail.com>
2024-03-08 13:25:06 +1300

    axios

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,8 +1,17 @@
+import axios from "axios"
+
 export const handler = async () => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'This is what will be returned!'
-		})
+	try {
+		const response = await axios.get('https://api.github.com/users/nlai659')
+		return {
+			statusCode: 200,
+			body: JSON.stringify(response.data)
+		}
+	
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: 'Failed fetching data' })
+		}
 	}
 }
