b230492cc79df6f4e35aadbfe837139f0da8a1b9
Nigel Lai <nigel.lai132@gmail.com>
2024-03-08 13:19:04 +1300

    super basic

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,9 +1,6 @@
 import fetch from "node-fetch";
 
 export const handler = async () => {
-  const res = await fetch("https://catfact.ninja/fact").then((res) => (res.json()));
-  console.log(res);
-
   return {
 	statusCode: 200,
 	body: "Hello, World!",
