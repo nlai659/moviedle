154bfc33b301d479a4ae933f951c3bbaf923fb1b
nlai659 <nlai659@aucklanduni.ac.nz>
2024-03-10 19:35:54 +1300

    Added: Logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import CategorySelector from '../game/CategorySelector';
+import Synopdle_Logo from '../../assets/s_logo_white.png';
 
 const Header: React.FC = () => {
     return (
         <header className="flex justify-between text-white py-4 px-6">
-            <h1 className="text-2xl font-bold">Synopdle</h1>
+            <div className="flex items-center">
+                <img src={Synopdle_Logo} alt="Synopdle Logo" className="w-10 h-10 mr-2" />
+                <h1 className="text-2xl font-bold">Synopdle</h1>
+            </div>
             <CategorySelector />
         </header>
     );
+    
 };
 
 export default Header;
